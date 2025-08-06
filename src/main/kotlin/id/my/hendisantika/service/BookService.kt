a449e08df13ceb27349afe9f134c0189a3475a20
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:39:13 +0700

    feat: Add BookService🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/service/BookService.kt
@@ -0,0 +1,14 @@
+package id.my.hendisantika.id.my.hendisantika.service
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ktor-ktorm-postgresql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/08/25
+ * Time: 10.39
+ * To change this template use File | Settings | File Templates.
+ */
+class BookService
