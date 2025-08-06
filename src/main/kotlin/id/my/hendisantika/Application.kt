59938b7f9e6c72cd49580a93f3a956fb93b40909
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:46:13 +0700

    feat: Modify Application.kt🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/Application.kt
+++ src/main/kotlin/id/my/hendisantika/Application.kt
@@ -1,10 +1,16 @@
 package id.my.hendisantika.id.my.hendisantika
 
+import id.my.hendisantika.id.my.hendisantika.config.configureSerialization
+import id.my.hendisantika.id.my.hendisantika.route.configureBookRoutes
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureBookRoutes()
+        configureSerialization()
+    }.start(wait = true)
 }
 
 fun Application.module() {
