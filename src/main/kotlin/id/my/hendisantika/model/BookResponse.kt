cdae28cd09a6dcf2c82f5cd8440fc49d46643533
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:38:40 +0700

    feat: Add BookResponse🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/model/BookResponse.kt
@@ -0,0 +1,20 @@
+package id.my.hendisantika.id.my.hendisantika.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ktor-ktorm-postgresql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/08/25
+ * Time: 10.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Serializable
+data class BookResponse(
+    val id: Long,
+    val name: String
+)
